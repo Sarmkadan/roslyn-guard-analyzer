c97e19e6532fdd564c16b408f5f9be0720cb320c
Vladyslav Zaiets <rutova2@gmail.com>
2024-12-04 09:42:26 +0200

    argument validation, SuppressRoslynGuardAttribute

--- src/RoslynGuardAnalyzer/Core/SuppressRoslynGuardAttribute.cs
+++ src/RoslynGuardAnalyzer/Core/SuppressRoslynGuardAttribute.cs
@@ -2,12 +2,27 @@ using System;
 
 namespace RoslynGuardAnalyzer
 {
+    /// <summary>
+    /// Attribute used to suppress RoslynGuard analysis for a specific class, method, or property.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
     public sealed class SuppressRoslynGuardAttribute : Attribute
     {
+        /// <summary>
+        /// Gets the ID of the rule being suppressed.
+        /// </summary>
         public string RuleId { get; }
+
+        /// <summary>
+        /// Gets or sets the justification for suppressing the rule.
+        /// </summary>
+        /// <param name="justification">The justification for suppressing the rule.</param>
         public string? Justification { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SuppressRoslynGuardAttribute"/> class.
+        /// </summary>
+        /// <param name="ruleId">The ID of the rule being suppressed.</param>
         public SuppressRoslynGuardAttribute(string ruleId)
         {
             RuleId = ruleId;
