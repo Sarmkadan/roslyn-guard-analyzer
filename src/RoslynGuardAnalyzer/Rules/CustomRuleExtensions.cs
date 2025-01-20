5ba3dd77aa54a9628c2662d5fcc7801858dd5b25
Vladyslav Zaiets <rutova2@gmail.com>
2025-01-20 18:30:49 +0200

    extension methods on CustomRule.

--- src/RoslynGuardAnalyzer/Rules/CustomRuleExtensions.cs
+++ src/RoslynGuardAnalyzer/Rules/CustomRuleExtensions.cs
@@ -20,18 +20,21 @@ public static class CustomRuleExtensions
     /// <summary>
     /// Evaluates a custom rule against the supplied code elements.
     /// </summary>
+    /// <param name="rule">The custom analysis rule to evaluate.</param>
+    /// <param name="elements">The code elements to analyze.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the list of rule violations found.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="rule"/> or <paramref name="elements"/> is <see langword="null"/>.</exception>
     public static Task<List<RuleViolation>> EvaluateAsync(this CustomAnalysisRule rule, IEnumerable<CodeElement> elements)
     {
-        if (rule is null)
-            throw new ArgumentNullException(nameof(rule));
-
-        if (elements is null)
-            throw new ArgumentNullException(nameof(elements));
+        ArgumentNullException.ThrowIfNull(rule);
+        ArgumentNullException.ThrowIfNull(elements);
 
         var violations = elements
-            .Where(rule.ViolationPredicate)
+            .Where(rule.ViolationPredicate ?? throw new InvalidOperationException("ViolationPredicate cannot be null"))
             .Select(element =>
             {
+                ArgumentNullException.ThrowIfNull(element);
+
                 var violation = new RuleViolation(rule.Id, rule.Name, rule.MessageFactory(element), element.FilePath)
                 {
                     LineNumber = element.StartLineNumber,
