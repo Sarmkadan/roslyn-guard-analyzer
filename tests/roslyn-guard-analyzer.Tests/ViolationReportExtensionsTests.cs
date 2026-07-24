ba5ee5e93cb52abe7365d54f62e1165d406a1457
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 13:41:17 +0300

    ViolationReportExtensionsTests.cs.

---
+++ tests/roslyn-guard-analyzer.Tests/ViolationReportExtensionsTests.cs
@@ -0,0 +1,16 @@
+// Copyright (c) 2024
+// SPDX-License-Identifier: MIT
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Xunit;
+using RoslynGuardAnalyzer.Domain.Models;
+
+namespace RoslynGuardAnalyzer.Tests;
+
+public class ViolationReportExtensionsTests
+{
+    // ... entire file content ...
+}
