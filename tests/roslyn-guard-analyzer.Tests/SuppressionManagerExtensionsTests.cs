cd84d7be93e7dbd16bb7398d74e0fc5fe58d9e26
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 16:16:01 +0300

    SuppressionManagerExtesnionsTests.cs

---
+++ tests/roslyn-guard-analyzer.Tests/SuppressionManagerExtensionsTests.cs
@@ -0,0 +1,140 @@
+using Xunit;
+using System.Collections.Generic;
+using System.Linq;
+using RoslynGuardAnalyzer.Suppressions;
+
+namespace RoslynGuardAnalyzer.Tests
+{
+    public class SuppressionManagerExtensionsTests
+    {
+        public SuppressionManagerExtensionsTests()
+        {
+        }
+
+        [Fact]
+        public void HappyPath_AddSuppression()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_NullInput_AddSuppression()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_AddSuppression()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_RemoveSuppressionsByRuleId()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_RemoveSuppressionsByRuleId()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_RemoveSuppressionsByRuleId()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_HasAnySuppressed()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_HasAnySuppressed()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_HasAnySuppressed()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_GetSuppressionCount()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_GetSuppressionCount()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_GetSuppressionCount()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_ExportActiveSuppressions()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_ExportActiveSuppressions()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_ExportActiveSuppressions()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_CleanupExpiredSuppressions()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_CleanupExpiredSuppressions()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_CleanupExpiredSuppressions()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void HappyPath_HasActiveSuppressionsForRule()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void EdgeCase_HasActiveSuppressionsForRule()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ErrorPath_HasActiveSuppressionsForRule()
+        {
+            // Test implementation here
+        }
+    }
+}
