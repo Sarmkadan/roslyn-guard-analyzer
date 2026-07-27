41bab981efffc8865de94021eb3acab44f9af734
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-27 08:31:53 +0300

    Tweak SuppressionRecordTests

---
+++ tests/roslyn-guard-analyzer.Tests/SuppressionRecordTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using RoslynGuardAnalyzer.Suppressions;
+
+namespace RoslynGuardAnalyzer.Tests
+{
+    public class SuppressionRecordTests
+    {
+        // TODO: Write tests for each major public method
+        // TODO: Test edge cases: null inputs, empty collections, boundary values
+        // TODO: Test error paths: expected exceptions
+    }
+}
