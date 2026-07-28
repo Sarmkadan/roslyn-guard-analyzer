368bce0d243524ea4f5b7bfea375b2dad6dd71a7
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-28 21:30:00 +0300

    chore(roslyn-guard-analyzer.tests): RoslynGuardAnalyzerOptionsTests chan

---
+++ tests/roslyn-guard-analyzer.Tests/RoslynGuardAnalyzerOptionsTests.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace RoslynGuardAnalyzer.Tests;
+
+public class RoslynGuardAnalyzerOptionsTests
+{
+    // ... entire file content ...
+}
