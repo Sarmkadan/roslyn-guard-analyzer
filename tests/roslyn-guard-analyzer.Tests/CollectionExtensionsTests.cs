72f96b9320f345f9da9ae110d501938be2c90146
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-30 07:18:14 +0300

    fix(roslyn-guard-analyzer.tests): unbreak CollectionExtensionsTests

---
+++ tests/roslyn-guard-analyzer.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using RoslynGuardAnalyzer.Utilities;
+
+namespace RoslynGuardAnalyzer.Tests
+{
+    public class CollectionExtensionsTests
+    {
+        [Fact]
+        public void BatchTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void DistinctByTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void AddIfNotNullTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void AddRangeIfNotNullTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void IsNullOrEmptyTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void OrEmptyTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void WithIndexTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void FirstOrNullTest()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public void ForEachTest()
+        {
+            // Test implementation here
+        }
+    }
+}
