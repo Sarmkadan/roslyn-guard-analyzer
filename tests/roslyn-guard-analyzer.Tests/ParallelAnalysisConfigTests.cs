79779cd23ec69c46d5f5d53dad343035f378007b
Vladyslav Zaiets <rutova2@gmail.com>
2026-08-05 03:03:51 +0000

    chore: apply workspace updates

---
+++ tests/roslyn-guard-analyzer.Tests/ParallelAnalysisConfigTests.cs
@@ -0,0 +1,41 @@
+using Xunit;
+using RoslynGuardAnalyzer.Services;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+
+namespace RoslynGuardAnalyzer.Tests
+{
+    public class ParallelAnalysisConfigTests
+    {
+        [Fact]
+        public async Task TestMaxDegreeOfParallelismAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestMaxRuleParallelismAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestAnalysisServiceAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestAnalyzeProjectAsync()
+        {
+            // Test implementation here
+        }
+
+        [Fact]
+        public async Task TestAnalyzeFileAsync()
+        {
+            // Test implementation here
+        }
+    }
+}
