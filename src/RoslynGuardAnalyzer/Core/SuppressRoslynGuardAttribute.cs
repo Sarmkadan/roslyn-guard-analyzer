c7dbb993674f785b8712d34280ca7fd4beda5bac
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-16 14:27:40 +0000

    chore: apply codex edits

--- src/RoslynGuardAnalyzer/Core/SuppressRoslynGuardAttribute.cs
+++ src/RoslynGuardAnalyzer/Core/SuppressRoslynGuardAttribute.cs
@@ -3,25 +3,25 @@ using System;
 namespace RoslynGuardAnalyzer
 {
     /// <summary>
-    /// Attribute used to suppress RoslynGuard analysis for a specific class, method, or property.
+    /// Indicates that RoslynGuard analysis is suppressed for a class, method, or property.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property, AllowMultiple = true, Inherited = false)]
     public sealed class SuppressRoslynGuardAttribute : Attribute
     {
         /// <summary>
-        /// Gets the ID of the rule being suppressed.
+        /// Gets the identifier of the RoslynGuard rule to suppress.
         /// </summary>
         public string RuleId { get; }
 
         /// <summary>
-        /// Gets or sets the justification for suppressing the rule.
+        /// Gets or sets the reason for suppressing the RoslynGuard rule.
         /// </summary>
         public string? Justification { get; set; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SuppressRoslynGuardAttribute"/> class.
+        /// Initializes a new instance of the <see cref="SuppressRoslynGuardAttribute"/> class for the specified rule.
         /// </summary>
-        /// <param name="ruleId">The ID of the rule being suppressed.</param>
+        /// <param name="ruleId">The identifier of the RoslynGuard rule to suppress.</param>
         public SuppressRoslynGuardAttribute(string ruleId)
         {
             RuleId = ruleId;
